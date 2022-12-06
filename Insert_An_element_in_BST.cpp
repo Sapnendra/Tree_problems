2ecfe80bbc4279e3b1ff2455ab1320c93a25d427
Sapnendra Jaiswal <116286507+Sapnendra@users.noreply.github.com>
2022-12-06 12:59:02 +0530

    Add files via upload

---
+++ Insert_An_element_in_BST.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<stdio.h>
+#include<malloc.h>
+using namespace std;
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* createNode(int data) {
+    struct node* n;
+    n = (struct node*) malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void insert(struct node* root, int key) {
+    struct node* prev = NULL;
+    while(root!=NULL) {
+        prev = root;
+        if(key==root->data) {
+            cout << "Cannot insert already in BST: "<< key << endl;
+            return;
+        }
+        else if(key<root->data) {
+            root = root->left;
+        }
+        else {
+            root = root->right;
+        }
+    }
+    struct node* newn = createNode(key);
+    if(key<prev->data) {
+        prev->left = newn;
+    }
+    else {
+        prev->right = newn;
+    }
+}
+
+int main() {
+    struct node *p = createNode(5);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
+
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    insert(p, 12);
+    cout << p->left->left->data;
+    return 0;
+
+}
